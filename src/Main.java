ea1696605e28aa92ed027ad33a4bb69189635733
NguyenDucVinh-18 <vnguyenduc187@gmail.com>
2025-03-13 07:50:38 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Task task = new Task("1", "Backend", "Description 1");
-        Person person1 = new Person("1", "Vinh", "vinh@gmail.com");
-        Person person2 = new Person("2", "Hai", "hai@gmail.com");
-        task.addPersonToTask(person1);
-        task.addPersonToTask(person2);
+        Stock Stock1 = new Stock("Iphone 14", 500);
+        Investor investor1 = new Investor("Vinh");
+        Investor investor2 = new Investor("Hai");
+        Investor investor3 = new Investor("Huy");
 
-        task.setName("Frontend");
+        Stock1.addObserver(investor1);
+        Stock1.addObserver(investor2);
+        Stock1.addObserver(investor3);
+
+        Stock1.setPrice(400);
+        System.out.println();
+        Stock1.setPrice(600);
+
 
     }
 }
